7b07a5c3c2fb03443b6555bfafbc19dfecab6120
Dylan Pinn <dylan@dylanpinn.com>
2018-06-15 01:03:06 +1000

    Add relations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,6 @@
 class Author < ApplicationRecord
+  has_many :books, through: :book_authors
+  has_many :book_authors
+
+  validates :name, presence: true
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+  has_many :authors, through: :book_authors
+  has_many :book_authors
+
+  validates :title, presence: true
 end
