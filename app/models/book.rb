86cdcee94d4b94358f960b0791dfa2d8034c309d
Dylan Pinn <dylan@dylanpinn.com>
2018-06-15 15:54:12 +1000

    Book requires at least 1 author

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ApplicationRecord
   has_many :authors, through: :book_authors
 
   validates :title, presence: true
+  validates :authors, presence: true
 
   accepts_nested_attributes_for :authors, reject_if: :all_blank, allow_destroy: true
 end
